14a3295ef7dc12fd433ca18d555a01cd99282ebc
swapnesh <swapnesha839@gmail.com>
2024-10-31 19:50:52 +0530

    update

--- app/ProtecteRouting.tsx
+++ app/ProtecteRouting.tsx
@@ -1,17 +1,17 @@
-// import Footer from '@/components/GlobalComponent/footer/Footer'
-// import Header from '@/components/GlobalComponent/Header/Header'
-// import Sidebar from '@/components/GlobalComponent/Sidebar/Sidebar'
+import Footer from '@/components/GlobalComponent/footer/Footer'
+import Header from '@/components/GlobalComponent/Header/Header'
+import Sidebar from '@/components/GlobalComponent/Sidebar/Sidebar'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import React from 'react'
 
 const ProtecteRouting = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='h-screen overflow-hidden flex'>
-      {/* <Sidebar /> */}
+      <Sidebar />
       <ScrollArea className='w-full'>
-        {/* <Header /> */}
+        <Header />
         {children}
-        {/* <Footer/> */}
+        <Footer/>
       </ScrollArea>
     </div>
   )
