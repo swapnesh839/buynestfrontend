d86de65b701c06bcfb235cfe70e5290fc8fdefd0
swapnesh <swapnesha839@gmail.com>
2024-11-02 10:39:10 +0530

    updated with better ed scene and properly integrated claudnary

--- app/glview/[id]/page.tsx
+++ app/glview/[id]/page.tsx
@@ -4,20 +4,16 @@ import React, { useEffect, useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { MeshReflectorMaterial, OrbitControls, PerspectiveCamera, useGLTF, useHelper } from "@react-three/drei";
 import { Mesh, Object3D, SpotLight, SpotLightHelper } from "three";
+import { useParams, useRouter } from "next/navigation";
+import { ArrowLeftCircle } from "lucide-react";
 // import { LinearEncoding } from "@react-three/drei/helpers/deprecated";
-
-const Model = () => {
+interface ModelProps {
+    scale: number,
+    MODEL_PATH: string
+}
+const ModelComponent: React.FC<ModelProps> = ({ scale, MODEL_PATH }) => {
     const ref = useRef<Mesh>(null);
-    // const { camera } = useThree()
-    const { scene } = useGLTF("/glb/sony_bluetooth_speaker.glb", true);
-    // useGLTF.preload('https://res.cloudinary.com/di7boajee/image/upload/v1730315920/monitor_speaker_jdmuzt.glb')
-    scene.scale.set(12, 12, 12); // Adjust scale if necessary
-    scene.position.set(0, -1, 0);
 
-    // const [roughnessMap, normalMap] = useLoader(TextureLoader, [
-    //     "/assets/textures/metal-siding-base-bl/metal-siding-base_roughness.png",
-    //     "/assets/textures/metal-siding-base-bl/metal-siding-base_normal-ogl.png",
-    // ])
     useEffect(() => {
         if (ref.current) {
             ref.current.traverse((object) => {
@@ -28,14 +24,6 @@ const Model = () => {
             });
         }
     }, [ref])
-    // useEffect(() => {
-    //     [roughnessMap, normalMap].forEach((map) => {
-    //         map.wrapS = RepeatWrapping;
-    //         map.wrapT = RepeatWrapping;
-    //         map.repeat.set(20, 20);
-    //     })
-    //     // normalMap.encoding = LinearE
-    // }, [roughnessMap, normalMap])
 
     const spotLightRef = useRef<SpotLight | null>(null);
     // useHelper(spotLightRef, SpotLightHelper, "hotpink");
@@ -44,8 +32,6 @@ const Model = () => {
     const [isInitialFlikkerdone, setisInitialFlikkerdone] = useState(false)
     const [InitialFlikkercount, setInitialFlikkercount] = useState(0)
     useFrame(() => {
-        // console.log(delta);        
-
         if (isInitialFlikkerdone) {
             if (lightintensity < 8) {
                 setlightintensity(lightintensity + 0.1)
@@ -62,27 +48,24 @@ const Model = () => {
             }
         }
     })
+    const { scene } = useGLTF(MODEL_PATH)
+    useEffect(() => {
+        if (scene) {
+            scene.scale.set(scale, scale, scale);
+        }
+    }, [scene, scale]);
+    // scene.position.set(0, 0, 0);
 
     return (
         <>
             <primitive ref={ref} object={scene} />
-            <PerspectiveCamera
-                makeDefault position={[3, 2, 5]}
-                fov={50} />
             <color attach="background" args={[0, 0, 0]} />
-            <ambientLight intensity={.05} />
-            {/* <spotLight castShadow
-                penumbra={1}
-                shadow-bias={-0.0001}
-                color={[1, 0.25, 0.7]}
-                position={[5, 5, 0]}
-                intensity={lightintensity}
-            /> */}
+
             <spotLight castShadow
                 // ref={spotLightRef}
                 penumbra={1}
                 shadow-bias={-0.0001}
-                color={[1, 0.25, 0.7]}
+                color={[1, 1, 1]}
                 position={[0, 5, 5]}
                 intensity={lightintensity}
             />
@@ -90,9 +73,25 @@ const Model = () => {
                 // ref={spotLightRef}
                 penumbra={1}
                 shadow-bias={-0.0001}
-                color={[1, 0.25, 0.7]}
+                color={[1, 1, 1]}
                 position={[0, 5, -5]}
                 intensity={lightintensity}
+            />
+            <spotLight castShadow
+                // ref={spotLightRef}
+                penumbra={1}
+                shadow-bias={-0.0001}
+                color={[1, 1, 1]}
+                position={[0, 1.5, 0]}
+                intensity={lightintensity*1}
+            />
+            <spotLight castShadow
+                // ref={spotLightRef}
+                penumbra={1}
+                shadow-bias={-0.0001}
+                color={[1, 0.25, 0.7]}
+                position={[0, 1.5, 0]}
+                intensity={lightintensity*.3}
             />
             <spotLight castShadow
                 // ref={spotLightRef}
@@ -151,190 +150,85 @@ const Model = () => {
                 intensity={lightintensity}
             />
 
-            {/* <spotLight
-                castShadow
-                intensity={lightintensity}
-                angle={0.6}
-                penumbra={1}
-                // // ref={spotLightRef}
-                position={[5, 5, 0]}
-                shadow-bias={-0.0001}
-                color={[1, 0.25, 0.7]}
-            /> */}
-            {/* <spotLight
-                castShadow
-                color={[0.14, 0.5, 1]}
-                intensity={lightintensity}
-                angle={0.6}
-                penumbra={1}
-                position={[-5, 5, 0]}
-                shadow-bias={-0.0001}
-            /> */}
-            {/* <spotLight
-                castShadow
-                color={[0.14, 1, 1]}
-                intensity={lightintensity}
-                angle={0.6}
-                penumbra={1}
-                position={[0, 5, 5]}
-                shadow-bias={-0.0001}
-            /> */}
-            {/* <spotLight
-                castShadow
-                intensity={lightintensity}
-                angle={0.6}
-                penumbra={1}
-                position={[5, 10, 5]} // Right and above
-                shadow-bias={-0.0001}
-                color={[1, 0.25, 0.7]}
-            /> */}
-            {/* <spotLight
-                castShadow
-                intensity={lightintensity}
-                angle={0.6}
-                penumbra={1}
-                position={[-5, 10, 5]} // Left and above
-                shadow-bias={-0.0001}
-                color={[0.14, 0.5, 1]}
-            /> */}
-            {/* <spotLight
-                castShadow
-                intensity={lightintensity}
-                angle={0.6}
-                penumbra={1}
-                position={[5, 10, -5]} // Right and back
-                shadow-bias={-0.0001}
-                color={[0.14, 1, 1]}
-            />
-            <spotLight
-                castShadow
-                intensity={lightintensity}
-                angle={0.6}
-                penumbra={1}
-                position={[-5, 10, -5]} // Left and back
-                shadow-bias={-0.0001}
-                color={[0.14, 1, 1]}
-            /> */}
-            <mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
-                <planeGeometry args={[30, 30]} />
-                <MeshReflectorMaterial
-                    envMapIntensity={0}
-                    // normalMap={normalMap}
-                    // normalMap-encoding={LinearEncoding}
-                    // roughnessMap={roughnessMap}
-                    dithering={true}
-                    mirror={0}
-                    blur={[1000, 300]}
-                    mixBlur={20}
-                    depthScale={0.01}
-                    minDepthThreshold={0.9}
-                    maxDepthThreshold={1}
-                    depthToBlurRatioBias={0.25}
-                    mixStrength={80}
-                    mixContrast={1}
-                    resolution={1024}
-                    metalness={0.5}
-                    reflectorOffset={0.2}
-                    roughness={.8} />
-            </mesh>
         </>
     )
 };
+const Model: React.FC<ModelProps> = React.memo(ModelComponent);
+
+const MODEL_PATH = "https://res.cloudinary.com/di7boajee/image/upload/v1730315920/monitor_speaker_jdmuzt.glb"
 
 const Page = () => {
-    return (
-        <div className="w-full h-full fixed inset-0 z-[1000] bg-black cursor-grab">
-            <Canvas
+    const containerRef2 = useRef<HTMLDivElement>(null)
+    const [scale, setScale] = useState<number>(1)
+    const { id } = useParams();
+    const router = useRouter()
+    // const { id } = params;
 
-                className="fixed top-0 left-0 right-0 bottom-0  w-full h-full"
-                camera={{ position: [2, 2, 3] }}
+    const debounce = <F extends (...args: unknown[]) => void>(func: F, delay: number): ((...args: Parameters<F>) => void) => {
+        let timeoutId: NodeJS.Timeout
+        return (...args: Parameters<F>) => {
+            clearTimeout(timeoutId)
+            timeoutId = setTimeout(() => func(...args), delay)
+        }
+    }
+    useEffect(() => {
+        const updateScale = () => {
+            if (containerRef2.current) {
+                const { width } = containerRef2.current.getBoundingClientRect()
+                const w = 300
+                const minDimension = Math.min(width, w)
+                setScale(minDimension * .25)
+            }
+        }
+
+        updateScale()
+        const handleResize = debounce(updateScale, 100)
+        window.addEventListener("resize", handleResize)
+
+        return () => window.removeEventListener("resize", handleResize)
+    }, [id])
+    return (
+        <div ref={containerRef2} className="w-full h-full fixed top-0 text-white left-0 right-0 bottom-0 z-[1000] bg-black cursor-grab">
+            <div onClick={() => { router.back() }} className="absolute bg-cyan-300 cursor-pointer p-1 text-black rounded-full z-[1002] top-[2%] left-[2%] flex justify-center align-middle">
+                <ArrowLeftCircle />
+            </div>
+            <Canvas
+                className="w-full h-full"
+            // camera={{ position: [2, 2, 3] }}
             >
-                <mesh>
+                {/* <mesh>
                     <meshStandardMaterial color={"red"} />
                     <boxGeometry args={[1, 2, 4]} />
+                </mesh> */}
+
+                <PerspectiveCamera
+                    makeDefault
+                    position={[0, 0, 0]}
+                    fov={50} />
+                <ambientLight intensity={.05} />
+                <mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
+                    <planeGeometry args={[60, 60]} />
+                    <MeshReflectorMaterial
+                        envMapIntensity={0}
+                        dithering={true}
+                        mirror={0}
+                        blur={[1000, 300]}
+                        mixBlur={20}
+                        depthScale={0.01}
+                        minDepthThreshold={0.9}
+                        maxDepthThreshold={1}
+                        depthToBlurRatioBias={0.25}
+                        mixStrength={80}
+                        mixContrast={1}
+                        resolution={1024}
+                        metalness={0.5}
+                        reflectorOffset={0.2}
+                        roughness={.8} />
                 </mesh>
                 <OrbitControls autoRotate={true} autoRotateSpeed={0.2} minDistance={2} maxDistance={10} maxPolarAngle={Math.PI / 2.4} minPolarAngle={Math.PI / 9} />
-                <Model />
-                {/* <CubeCamera resolution={256} frames={Infinity}>
-                    {(texture) => (
-                        <>
-                        <Environment map={texture} />
-                        </>
-                    )}
-                </CubeCamera> */}
-                {/* <TorosGeos /> */}
+                <Model MODEL_PATH={MODEL_PATH} scale={scale} />
             </Canvas>
         </div>
     );
 };
-// const TorosGeos = () => {
-//     const [colors, setcolors] = useState("hsl(0, 100%, 50%)")
-//     const [order, setorder] = useState(true)
-//     const torosRef: MutableRefObject<(Mesh | null)[]> = useRef([]);
-//     // const [colors, setcolors] = useState({ one: "rgb(255, 0, 0)", two: "rgb(255, 0, 0)", three: "rgb(255, 0, 0)", four: "rgb(255, 0, 0)", five: "rgb(255, 0, 0)" })
-//     let [clrref, setcolorref] = useState(0)
-//     useFrame(() => {
-//         // const ramdomval = Math.ceil(Math.random() * 2)
-//         // // console.log(delta,ramdomval);
-//         // console.log(colors);
-//         // setcolors(`hsl(${clrref}, 100%, 50%)`)
-//         // if (order) {
-//         //     setcolorref(clrref + ramdomval)
-//         // } else {
-//         //     setcolorref(clrref - ramdomval)
-//         //     // setcolors(`hsl(${clrref-ramdomval}, 100%, 50%)`)
-//         // }
-
-//         // for (let i = 0; i < torosRef.current.length; i++) {
-//         //     let mesh = torosRef.current[i]
-
-//         //     let z = Math.abs((i - 7) * 3.5)
-//         //     mesh?.position.set(0, 0, z)
-//         //     mesh?.scale.set(1 - z * .04, 1 - z * .04, 1 - z * .04)
-//         //     let colorscale = 1
-//         //     if (z > 2) {
-//         //         colorscale = 1 - (Math.min(z, 12) - 2) / 10
-//         //     }
-//         //     colorscale *= .5
-//         //     if (i % 2 == 1) {
-//         //         if (mesh?.material instanceof MeshStandardMaterial) {
-//         //             mesh.material.emissive = new Color(6, 0.16, 4).multiplyScalar(colorscale)
-//         //         }
-//         //     } else {
-//         //         if (mesh?.material instanceof MeshStandardMaterial) {
-//         //             mesh.material.emissive = new Color(.2, 0.6, 3).multiplyScalar(colorscale)
-//         //         }
-//         //     }
-
-//         // }
-
-//     })
-//     useEffect(() => {
-//         const interval = setInterval(() => {
-//             if (order) {
-//                 setorder(false)
-//             } else {
-//                 setorder(true)
-//             }
-//         }, 3000);
-//         return (
-//             clearInterval(interval)
-//         )
-//     }, [order])
-//     return (
-//         <>
-//             {
-//                 Array(14).fill(0).map((v, i) => {
-//                     return (<mesh
-//                         ref={(ref) => { torosRef.current[i] = ref }}
-//                         key={i} position={[0, 0, 0]}>
-//                         <torusGeometry args={[1.2, 0.13, 160, 100]} />
-//                         <meshStandardMaterial emissive={[0.5, 0.5, 0.5]} color={[0, 0, 0]} />
-//                     </mesh>)
-//                 })
-//             }
-//         </>
-//     )
-// }
-
-export default Page;
+export default React.memo(Page);
